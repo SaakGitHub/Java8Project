9ddb014dcfd542f496691628f3c6fc386475e631
Saakib <saakib148@gmail.com>
2023-04-04 18:10:15 +0530

    src Updated

--- src/main/java/com/lambda/ThreadFunctional.java
+++ src/main/java/com/lambda/ThreadFunctional.java
@@ -1,10 +1,44 @@
 package com.lambda;
+/*
+ *Normal Approach 
+ *
+class MyThread implements Runnable {
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName());
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child Thread");
+		}
+	}
+}
+public class ThreadFunctional {
+	public static void main(String[] args) {
+		MyThread r = new MyThread();
+		Thread t = new Thread(r);
+		System.out.println(t.currentThread().getName());
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Parent Thread");
+		}
+	}
+}
+*/
+
+/*Functional Interface Approach
+
+*/
 
 public class ThreadFunctional {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Runnable r = () -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Lambda Child Thread");
+			}
+		};
+		Thread t = new Thread(r);
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Lambda Parent Thread");
+		}
 	}
-
 }
